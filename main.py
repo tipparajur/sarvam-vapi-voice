b9a33700f3af1c5e4b6141b6ff68f7b88dc744be
tipparajur <87379056+tipparajur@users.noreply.github.com>
2026-06-12 14:53:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
-from fastapi import Request
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import requests
+import os
 
-@app.post("/tts")
-async def generate_speech(request: Request):
+load_dotenv()
 
-    body = await request.json()
-    print("VAPI BODY:", body)
+app = FastAPI()
 
-    text = (
-        body.get("text")
-        or body.get("message")
-        or body.get("input")
-        or body.get("transcript")
-        or "Hello"
-    )
+SARVAM_API_KEY = os.getenv("SARVAM_API_KEY")
+
+class SpeechRequest(BaseModel):
+    text: str
+    language: str = "te-IN"
+
+@app.get("/")
+def home():
+    return {"status": "running"}
+
+@app.get("/test")
+def test():
+    return {
+        "api_key_loaded": SARVAM_API_KEY is not None
+    }
+@app.get("/test-tts")
+def test_tts():
 
     url = "https://api.sarvam.ai/text-to-speech"
 
@@ -22,8 +34,32 @@ async def generate_speech(request: Request):
     }
 
     payload = {
-        "text": text,
-        "target_language_code": "te-IN"
+        "text": "నమస్కారం. ఇది సర్వమ్ తెలుగు వాయిస్ పరీక్ష.",
+        "target_language_code": "te-IN",
+        "model": "bulbul:v3"
+    }
+
+    response = requests.post(
+        url,
+        headers=headers,
+        json=payload
+    )
+
+    return response.json()
+
+@app.post("/tts")
+def generate_speech(request: SpeechRequest):
+
+    url = "https://api.sarvam.ai/text-to-speech"
+
+    headers = {
+        "api-subscription-key": SARVAM_API_KEY,
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "text": request.text,
+        "target_language_code": request.language
     }
 
     response = requests.post(
